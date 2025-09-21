6778c6ad55d08dd91e00620bb14ce3715e085b63
unknown <deyllonp986@gmail.com>
2025-09-20 23:19:35 -0300

    try to use https

--- assite-ai/src/main.ts
+++ assite-ai/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as fs from 'fs';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const httpsOptions = {
+    key: fs.readFileSync('./ssl/nodeapp.key'),
+    cert: fs.readFileSync('./ssl/nodeapp.crt'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+  await app.listen(3000);
 }
+
 bootstrap();
